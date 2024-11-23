a060a8df59421672a59b118fe4ee10df8a2d1202
Daniel Hall <beta@hai.haus>
2024-11-22 16:09:17 -0800

    feat: Added ping route

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Pong!"))
+		})
+	})
+
+	log.Println("Serving on :3000 ...")
+	http.ListenAndServe(":3000", r)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/betapictoris/paranormal
 
 go 1.18
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
